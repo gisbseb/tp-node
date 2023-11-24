a259b9f13a9b6bf10724b68db15d996bc6f59a70
gisbseb <gisbseb@gmail.com>
2023-11-24 12:18:50 +0100

    front

---
+++ router/routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("template/register", { title: "Inscription" });
+});
+
+router.get("/login", (req, res) => {
+  res.render("template/login", { title: "Connexion" });
+});
+
+router.get("/dashboard", (req, res) => {
+  res.render("template/dashboard", { title: "Dashboard" });
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-//Init
+import express from "express";
+import dotenv from "dotenv";
+import { join } from "node:path";
+import router from "./router/routes.js";
+
+dotenv.config();
+const { PORT, HOST } = process.env;
+
+const app = express();
+const cwd = process.cwd();
+
+app.set("view engine", "pug");
+const staticPath = join(cwd, "public");
+app.use(express.static(staticPath));
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://${HOST}:${PORT}`);
+});
