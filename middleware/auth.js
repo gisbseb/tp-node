017640b10a8603653f3480f56e9007a7f3ac93ef
Jo-han <jo-han@miloctav.fr>
2023-11-24 17:00:39 +0100

    flash msg on invalid tokens

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,26 @@
 import jwt from 'jsonwebtoken'
+import renderController from '../controller/render.controller.js';
 
 const verifyToken = async (req, res, next) => {
     const token = req.session.user?.token
-    if (!token) return res.status(401).send('No token')
+    if (!token) {
+        req.session.flash = {
+            class: "failed",
+            message: "Vous n'êtes pas connecté",
+        };
+        renderController.loginRender(req, res)
+    }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.user = decoded;
+        next()
     } catch (err) {
-        return res.status(400).send('Invalid token')
+        req.session.flash = {
+            class: "failed",
+            message: "Token invalide",
+        };
+        renderController.loginRender(req, res)
     }
-    next()
 }
 
 export default { verifyToken }
