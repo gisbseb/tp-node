931341c60fbd61633e581fc497f0c67bf0f21ffc
Jo-han <jo-han@miloctav.fr>
2023-11-24 11:52:11 +0100

    init backend

---
+++ model/User.model.js
@@ -0,0 +1,24 @@
+import { mongoose } from "./db.js";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export { User };
--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-//Init
+import { User } from "./model/User.model.js";
+
+const test = new User({
+    firstName: 'Test2',
+    lastName: 'User',
+    email: 'test@test.com',
+    password: '1234567890'
+});
+
+await test.save();
