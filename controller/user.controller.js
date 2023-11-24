282844fa8155211c6740e4d94b1252a10a0e2fb9
gisbseb <gisbseb@gmail.com>
2023-11-24 15:43:16 +0100

    ok

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,59 +4,59 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const login = async (req, res) => { 
-    console.log(req.body)
-    const { email, password } = req.body;
-    const sha256Hasher = crypto.createHmac("sha256", process.env.SECRET_HASH);
-    const hashedPwd = sha256Hasher.update(password).digest("hex");
+const login = async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  const sha256Hasher = crypto.createHmac("sha256", process.env.SECRET_HASH);
+  const hashedPwd = sha256Hasher.update(password).digest("hex");
 
-    let existingUser = await User.findOne({ email, password: hashedPwd });
-    if (!existingUser) {
-        return res.status(401).send({
-            message: "Invalid credentials"
-        });
-    }
+  let existingUser = await User.findOne({ email, password: hashedPwd });
+  if (!existingUser) {
+    return res.status(401).send({
+      message: "Invalid credentials",
+    });
+  }
 
-    return res.redirect("/dashboard");
+  return res.redirect("/dashboard");
 };
 const find = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.send(users);
-    } catch (err) {
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving users."
-        });
-    }
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving users.",
+    });
+  }
 };
 const create = async (req, res) => {
-    try {
-        const { email, password, lastName, firstName } = req.body;
+  try {
+    const { email, password, lastName, firstName } = req.body;
 
-        let existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(409).send({
-                message: "User already exists"
-            });
-        }
-
-        const sha256Hasher = crypto.createHmac("sha256", process.env.SECRET_HASH);
-        const hashedPwd = sha256Hasher.update(password).digest("hex");
-        await User.create({
-            email,
-            password: hashedPwd,
-            lastName,
-            firstName,
-        });
-
-        res.redirect("/login");
-    } catch (err) {
-        res.status(500).send({
-            message: err.message || "Some error occurred while creating the User."
-        });
+    let existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).send({
+        message: "User already exists",
+      });
     }
+
+    const sha256Hasher = crypto.createHmac("sha256", process.env.SECRET_HASH);
+    const hashedPwd = sha256Hasher.update(password).digest("hex");
+    await User.create({
+      email,
+      password: hashedPwd,
+      lastName,
+      firstName,
+    });
+
+    res.redirect("/login");
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the User.",
+    });
+  }
 };
-const update = async (req, res) => { };
-const remove = async (req, res) => { };
+const update = async (req, res) => {};
+const remove = async (req, res) => {};
 
 export default { login, find, create, update, remove };
